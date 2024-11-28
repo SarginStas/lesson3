4eab79302517e83ddd1d80da53a9c1e2c22365c2
DmitriyK37 <qsc1986@mail.ru>
2024-11-28 21:59:51 +0300

    added test for id

--- src/test/java/tests/BookingApiTestDK.java
+++ src/test/java/tests/BookingApiTestDK.java
@@ -7,11 +7,7 @@ import io.restassured.response.Response;
 import models.*;
 import models.BookingId;
 import utils.JsonUtils;
-
 import org.testng.annotations.Test;
-
-import java.util.Arrays;
-
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -22,25 +18,6 @@ import static org.testng.AssertJUnit.assertTrue;
 public class BookingApiTestDK extends TestConfig {
 
     private final BookingApi bookingApi = new BookingApi();
-//
-//    @Test
-//    @Story("Get Booking")
-//    @Description("Verify that a booking can be retrieved")
-//    @Severity(SeverityLevel.CRITICAL)
-//    public void getBooking() {
-//        Booking booking = bookingApi.getBooking(100);
-//
-//        System.out.println(booking.getFirstname());
-//
-//        assertNotNull(booking.getFirstname());
-//        assertEquals(booking.getFirstname(), "Josh");
-//        assertEquals(booking.getLastname(), "Allen");
-//        assertEquals(booking.getTotalprice(), 111);
-//        assertEquals(booking.isDepositpaid(), true);
-//        assertEquals(booking.getBookingdates().getCheckout(), "2019-01-01");
-//        assertEquals(booking.getBookingdates().getCheckin(), "2018-01-01");
-//        assertEquals(booking.getAdditionalneeds(), "midnight snack");
-//    }
 
     @Test
     @Story("Get Bookings")
@@ -61,9 +38,7 @@ public class BookingApiTestDK extends TestConfig {
         assertEquals(response.getStatusCode(), 200);
 
         // Deserialize the response into an array of BookingId objects
-
         BookingId[] bookingIds = JsonUtils.fromJson(response.asString(), BookingId[].class);
-        System.out.println(Arrays.toString(bookingIds));
 
         assertNotNull(bookingIds);
         assertTrue(bookingIds.length > 0);
@@ -80,10 +55,9 @@ public class BookingApiTestDK extends TestConfig {
 
             // Проверяем соответствие имени и фамилии
             assertEquals(booking.getFirstname(), firstName,
-                    "First name mismatch for booking ID: " + id);
+                    "Firstname mismatch for booking ID: " + id);
             assertEquals(booking.getLastname(), lastName,
-                    "Last name mismatch for booking ID: " + id);
-
+                    "Lastname mismatch for booking ID: " + id);
             System.out.println("Validated booking with ID: " + id);
         }
     }
