205273c798d5df9823c014fd26e8bd2737eee5bf
Aira416 <nastaevamn@gmail.com>
2024-12-08 22:44:05 +0300

    Done

--- src/test/java/tests/BookingApiTestMN.java
+++ src/test/java/tests/BookingApiTestMN.java
@@ -10,16 +10,16 @@ import static org.testng.Assert.*;
 
 @Epic("REST API Tests")
 @Feature("Booking Management")
-public class BookingApiTest extends TestConfig {
+public class BookingApiTestMN extends TestConfig {
     
     private final BookingApi bookingApi = new BookingApi();
-    
+
     @Test
     @Story("Get Booking")
     @Description("Verify that a booking can be retrieved")
     @Severity(SeverityLevel.CRITICAL)
     public void getBooking() {
-        Booking booking = bookingApi.getBooking(100);
+        Booking booking = bookingApi.getBooking(111);
 
         System.out.println(booking.getBookingdates());
 
@@ -28,6 +28,8 @@ public class BookingApiTest extends TestConfig {
         assertEquals(booking.getLastname(), "Smith");
         assertEquals(booking.getTotalprice(), 111);
         assertTrue(booking.isDepositpaid());
-
+        assertEquals(booking.getBookingdates().getCheckin(),"2018-01-01");
+        assertEquals(booking.getBookingdates().getCheckout(),"2019-01-01");
+        assertEquals(booking.getAdditionalneeds(), "Breakfast");
     }
 } 
